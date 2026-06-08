5d58637c9fd65cc92f53a86ca0a559cd1b873d32
IvanShankin <ivan_shankin06@mail.ru>
2026-06-08 11:10:27 +0300

    added error type in model WebsiteChecks

--- src/models/website_check/models.py
+++ src/models/website_check/models.py
@@ -1,7 +1,18 @@
-from sqlalchemy import Column, Integer, String, func, DateTime, ForeignKey, Boolean, text
+import enum
+
+from sqlalchemy import Column, Integer, String, func, DateTime, ForeignKey, Boolean, text, Enum
 from sqlalchemy.orm import relationship
 
 from src.core.database import Base
+
+
+class ErrorType(enum.Enum):
+    TIMEOUT = "timeout"
+    DNS_ERROR = "dns_error"
+    SSL_ERROR = "ssl_error"
+    CONNECTION_ERROR = "connection_error"
+    HTTP_ERROR = "http_error"
+
 
 class WebsiteChecks(Base):
     __tablename__ = "website_checks"
@@ -11,7 +22,17 @@ class WebsiteChecks(Base):
     http_status_code = Column(Integer, nullable=True)
     response_time_ms = Column(Integer, nullable=False)
     is_available = Column(Boolean, nullable=False)
+
+    error_type = Column(
+        Enum(
+            ErrorType,
+            values_callable=lambda x: [e.value for e in x],
+            name="error_type"
+        ),
+        nullable=True
+    )
     error_message = Column(String, nullable=False)
+
     checked_at = Column(DateTime(timezone=True), server_default=func.now())
 
     website = relationship("Websites", back_populates="website_checks")
